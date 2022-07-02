89660d5c950d7218ccd94be95813091c074a6390
aota18 <sapphire031794@gmail.com>
2022-07-02 13:30:00 +0900

    add lock file

--- contracts/SimpleStaking.sol
+++ contracts/SimpleStaking.sol
@@ -101,6 +101,38 @@ contract SmartStaking {
     /// @param token, the official ERC20 token which this contract exclusively accepts.
     /// @param amount to allocate to recipient.
     function stakeTokens(IERC20 token, uint256 amount) public timestampIsSet noReentrant {
-        
+        require(token == erc20Contract, "You are only allowed to stake toe official erc20 token address which was passed into this contract's consturctor");
+        require(amount <= token.balanceOf(msg.sender), "Not enough STATE tokens in your wallet, please try lesser amount");
+        token.safeTransferFrom(msg.sender, address(this), amount);
+        balances[msg.sender] = balances[msg.sender].add(amount);
+        emit tokensStaked(msg.sender, amount);
+    }
+
+    /// @dev Allows user to unstake tokens after the correct time period has elapsed
+    /// @param token - address of the official ERC20 token which is being unlocked here.
+    /// @param amount - the amount to unlock (in wei)
+    function unstakeTokens(IERC20 token, uint256 amount) public timestampIsSet noReentrant {
+        require(balances[msg.sender] >= amount, "Insufficient token balance, try lesser amount");
+        require(token == erc20Contract, "Token parameter must be the same as the erc20 contract address which was passed into the constructor");
+        if (block.timestamp >= timePeriod) {
+            alreadyWithdrawn[msg.sender] = alreadyWithdrawn[msg.sender].add(amount);
+            balances[msg.sender] = balances[msg.sender].sub(amount);
+            token.safeTransfer(msg.sender, amount);
+            emit TokensUnstaked(msg.sender, amount);
+        } else {
+            revert("Tokens are only available after correct time period has elapsed");
+        }
+    }
+
+
+      /// @dev Transfer accidentally locked ERC20 tokens.
+    /// @param token - ERC20 token address.
+    /// @param amount of ERC20 tokens to remove.
+    function transferAccidentallyLockedTokens(IERC20 token, uint256 amount) public onlyOwner noReentrant {
+        require(address(token) != address(0), "Token address can not be zero");
+        // This function can not access the official timelocked tokens; just other random ERC20 tokens that may have been accidently sent here
+        require(token != erc20Contract, "Token address can not be ERC20 address which was passed into the constructor");
+        // Transfer the amount of the specified ERC20 tokens, to the owner of this contract
+        token.safeTransfer(owner, amount);
     }
 }
